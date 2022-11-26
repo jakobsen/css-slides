88bae04ca70befde7c407984adab2bdf0638fdfb
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2022-11-26 16:05:51 +0100

    Add icons

---
+++ src/components/icons/ChevronRight.tsx
@@ -0,0 +1,22 @@
+import type { IconProps } from "./iconTypes"
+
+export default function ChevronRight({
+  size = 24,
+  color = "currentColor",
+}: IconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={color}
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <polyline points="9 18 15 12 9 6"></polyline>
+    </svg>
+  )
+}
