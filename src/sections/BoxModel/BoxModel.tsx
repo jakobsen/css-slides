d4a98e733a93e635bf3593aa95d76dc54c50e6a0
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2022-11-27 12:30:48 +0100

    WIP: BoxModel section

---
+++ src/sections/BoxModel/BoxModel.tsx
@@ -0,0 +1,26 @@
+import BoxModelDemo from "@components/BoxModelDemo"
+import Slide from "@components/Slide"
+import styles from "./BoxModel.module.css"
+
+export default function BoxModelSection() {
+  return (
+    <>
+      <Slide>
+        <h1 style={{ fontSize: 64, fontWeight: 600 }}>Boksmodellen</h1>
+      </Slide>
+      <Slide>
+        <BoxModelDemo />
+      </Slide>
+      <Slide>
+        <div className={styles.codeblock}>
+          <span className={styles.selector}>*</span>{" "}
+          <span className={styles.bracket}>&#123;</span> <br />
+          &nbsp;&nbsp;<span className={styles.property}>box-sizing</span>:{" "}
+          <span className={styles.value}>border-box</span>;
+          <br />
+          <span className={styles.bracket}>&#125;</span>
+        </div>
+      </Slide>
+    </>
+  )
+}
